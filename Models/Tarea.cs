c1bbc8103948601cda903f55599681fa222485e6
Adan Rainerio López Maradiaga <156128309+arlopez-android@users.noreply.github.com>
2024-01-31 15:36:41 -0600

    Agregar archivos de proyecto.

---
+++ Models/Tarea.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace prueba.Models;
+
+public class Tarea
+{
+    [Key]
+    public Guid TareaId { get; set; }
+
+    [ForeignKey("CategoriaId")]
+    public Guid CategoriaId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Titulo { get; set; }
+    public string Descripcion { get; set; }
+    public Prioridad PrioridadTarea { get; set; }
+    public DateTime FechaCreacion { get; set; }
+    public virtual Categoria Categoria { get; set; }
+
+    [NotMapped] // Permite omitir alguna propiedad que posee nuestra clase que no queremos que se cree en la DB
+    public string Resumen {  get; set; }
+}
+
+public enum Prioridad
+{
+    Baja,
+    Media,
+    Alta
+}
---
+++ TareasContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prueba.Models;
+
+namespace prueba;
+
+public class TareasContext : DbContext
+{
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Tarea> Tareas { get; set; }
+
+    public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+}

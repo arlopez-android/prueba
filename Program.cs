770647facc6435ed04f0870ee1a70ffd74272f77
Adan Rainerio López Maradiaga <156128309+arlopez-android@users.noreply.github.com>
2024-01-31 16:04:57 -0600

    Primer endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prueba;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello Sexy Fat!");
+
+app.MapGet("/dbconection", async ([FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+
+    return Results.Ok($"Base de datos en memoria: {dbContext.Database.IsInMemory()}");
+});
 
 app.Run();
